0909ea1fab59f177bca9d0c0825a3dcc1f26c1cb
Cybercalf <cybercalf@163.com>
2021-07-30 13:15:42 +0800

    删除冗余代码

--- HwcPlayer/Buf.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include "Glue.h"
-
-extern HANDLE hOutput, hOutBuf;
-//两个句柄，这里可以就理解为我们等会需要用的两个缓冲区
-extern COORD coord;  //位置坐标，用到的时候解释
-extern DWORD bytes;        //这个主要是要配合下面的一个函数
-void initconsoleScreenBuffer();     //初始化我们的缓存区
